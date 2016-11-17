2ed4dbcf79976609ed62a73331888ff3a46d70e4
Michaël Vanbrabandt <info@mivaweb.be>
2016-11-17 17:00:52 +0100

    Create custom url provider

---
+++ src/Umbraco.Web.UI/Events/CustomEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Web.Routing;
+using Umbraco.Web.UI.Providers;
+
+namespace Umbraco.Web.UI.Events
+{
+    public class CustomEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            UrlProviderResolver.Current.InsertTypeBefore<DefaultUrlProvider, CustomUrlProvider>();
+        }
+    }
+}
---
+++ src/Umbraco.Web.UI/Providers/CustomUrlProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Configuration.UmbracoSettings;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Web.UI.Providers
+{
+    public class CustomUrlProvider : UrlProviderBase
+    {
+        //public CustomUrlProvider(IRequestHandlerSection requestSettings): base(requestSettings) { }
+
+        public override IEnumerable<string> GetOtherUrls(UmbracoContext umbracoContext, int id, Uri current)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUrl(UmbracoContext umbracoContext, int id, Uri current, UrlProviderMode mode)
+        {
+            var domainHelper = new DomainHelper(umbracoContext.Application.Services.DomainService);
+
+            return AssembleUrl(domainHelper.DomainForNode(id, current), "/custom-url", current, mode).ToString();
+        }
+    }
+}
